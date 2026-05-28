bb4f08135fc60a8ee56dfd4eb28147eb5854fb7b
rproyecto846-jpg <rproyecto846@gmail.com>
2026-05-28 16:03:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { Server } = require("socket.io");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: { origin: "*" }
