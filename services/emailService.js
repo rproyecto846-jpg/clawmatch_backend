60c5f8a753e2fde41f6302b9bd2a3b793161c72d
rproyecto846-jpg <rproyecto846@gmail.com>
2026-05-28 17:16:00 +0200

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 exports.enviarEmail = async (destinatario, asunto, html) => {
     await resend.emails.send({
         from: "ClawMatch <onboarding@resend.dev>",
-        to: destinatario,
+        to: "rproyecto846@gmail.com", 
         subject: asunto,
         html,
     });
