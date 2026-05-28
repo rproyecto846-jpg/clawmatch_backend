bc0edbe9e3d6d8630ff8f87a433cfa65d836a4cd
rproyecto846-jpg <rproyecto846@gmail.com>
2026-05-28 16:05:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ socket.on("dejo_escribir_comunidad", ({ id_comunidad }) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
