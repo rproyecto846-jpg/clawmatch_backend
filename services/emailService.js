900c80b720803f3b3cd1f3579e5cad4f1ebaecb0
rproyecto846-jpg <rproyecto846@gmail.com>
2026-05-28 17:47:07 +0200

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -1,14 +1,12 @@
 const Brevo = require("@getbrevo/brevo");
-
 const client = Brevo.ApiClient.instance;
 client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
-
 const apiInstance = new Brevo.TransactionalEmailsApi();
 
 exports.enviarEmail = async (destinatario, asunto, html) => {
     const email = new Brevo.SendSmtpEmail();
     email.to = [{ email: destinatario }];
-    email.sender = { email: "noreply@clawmatch.onrender.com", name: "ClawMatch" };
+    email.sender = { email: "rproyecto846@gmail.com", name: "ClawMatch" };
     email.subject = asunto;
     email.htmlContent = html;
     await apiInstance.sendTransacEmail(email);
